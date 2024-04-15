1ae1e727da5157a8cee48e5ef1e05bad7ab5f3d5
Ar57m <132871733+Ar57m@users.noreply.github.com>
2024-04-15 20:00:22 -0300

    Update array_to_midi.py

--- array_to_midi.py
+++ array_to_midi.py
@@ -7,13 +7,12 @@ from midiutil import MIDIFile
 # for now it's only converting the prime numbers array to midi
 
 def note_to_midi(note):
-    
-    map = {'C': 60, 'D': 62, 'E': 64, 'F': 65, 'G': 67, 'A': 69, 'B': 71}
-    
-    note_name, octave = note[:-1], int(note[-1])
-    
-    midi = map[note_name] + (octave - 4) * 12
-    return midi
+
+    note_map = {'C3': 48, 'D3': 50, 'E3': 52, 'F3': 53, 'G3': 55, 'A3': 57, 'B3': 59, 'C4': 60,  'C#4': 61, 'D4': 62,  'D#4': 63, 'E4': 64, 'F4': 65,  'F#4': 66, 'G4': 67,  'G#4': 68, 'A4': 69,  'A#4': 70, 'B4': 71, 'C5': 72, 'D5': 74, 'E5': 76, 'F5': 77, 'G5': 79, 'A5': 81, 'B5': 83 }
+    note_name, octave = note , int(note[-1])
+    midi_note = note_map[note_name] + (octave - 4) * 12
+  
+    return midi_note
 
 
 def array_to_mid(array, name):
@@ -21,8 +20,9 @@ def array_to_mid(array, name):
     notes = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4']
 
     midi = MIDIFile(1)  # 1 Track
-    time = 110
-    midi.addProgramChange(0, 0, 0, 41) # 41 is violin
+    time = 154
+    duration = 1
+    midi.addProgramChange(0, 0, 0, 1) # 1 I think is bright grand
     
     midi.addTempo(0, 0, time)
     te = array.reshape(-1)//7
@@ -30,13 +30,13 @@ def array_to_mid(array, name):
     
     for i, num in enumerate(array-1):
         
-        position = num % 7
+        position = int(num % 7)
         
         note = notes[position]
         
         midi_number = note_to_midi(note)
         
-        midi.addNote(0, 0, midi_number, te[i] *0.7, 0.7, 100)  # track, channel, pitch, start_time, duration and volume 
+        midi.addNote(0, 0, midi_number, te[i] *duration, duration, 127)  # track, channel, pitch, start_time, duration and volume 
 
     with open(name, 'wb') as file:
         midi.writeFile(file)
